49018ae78720b0afcaaa110456a1ea5cc1699b89
Gabriel Lococo <127756253+GabrielLococo@users.noreply.github.com>
2024-01-08 17:17:32 -0300

    adding addProductToCart

--- src/cartManager.js
+++ src/cartManager.js
@@ -36,4 +36,27 @@ export class CartManager {
         return newCart
     }
 
+    addProductToCart = async (cart_id, product_id) => {
+        const carts = await this.getCarts()
+        const index = carts.findIndex(cart => cart.id === cart_id)
+
+        if(index !== -1){
+            const cartProducts = await this.getCartProducts(cart_id)
+            const existingProductIndex = cartProducts.findIndex(product => product.product_id === product_i)
+            
+            if(existingProductIndex !== -1){
+                cartProducts[existingProductIndex].quantity = cartProducts[existingProductIndex].quantity + 1
+            }else {
+                cartProducts.push({product_id, quantity : 1})
+            }
+
+            carts[index].products = cartProducts
+
+            await fs.writeFile(this.path, JSON.stringify(carts))
+            console.log('producto agregado con exito')
+        } else {
+            console.log('carrito no encontrado')
+        }
+    }
+
 }
